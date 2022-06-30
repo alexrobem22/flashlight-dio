939a7751280e49116c204543a7bbf605689410f6
alexvieira <alexvieira@audax.mobi>
2022-06-30 00:17:21 -0300

    falta termina fiquei sem tempo

--- App.js
+++ App.js
@@ -1,20 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import  React , {useState, useEffect} from 'react';
+import {View, Image, StyleSheet, TouchableOpacity} from 'react-native';
+
+
 
 export default function App() {
+  const [toggle, setToggle] = useState(false);
+
+  const handleChangeToggle = () => setToggle(oldToggle => !oldToggle);
+
+  useEffect(()=>{
+   
+  }, [toggle]);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={toggle ? styles.containerLigth : styles.container}>
+
+      <TouchableOpacity onPress={handleChangeToggle}>
+
+      <Image style={toggle ? styles.lightinOn : styles.lightinOff} source={toggle ? require('./assets/icone-dio/eco-light.png') : require('./assets/icone-dio/eco-light-off.png')} />
+     
+      <Image style={styles.dioLogo} source={toggle ? require('./assets/icone-dio/logo-dio.png') : require('./assets/icone-dio/logo-dio-white.png')} />
+     
+      </TouchableOpacity>
+
     </View>
+
+    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  containerLigth: {
+    flex: 1,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  lightinOn: {
+    resizeMode: 'contain',
+    alignSelf: 'center',
+    width: 150,
+    height: 150,
+  },
+  lightinOff: {
+    resizeMode: 'contain',
+    alignSelf: 'center',
+    tintColor: 'white',
+    width: 150,
+    height: 150,
+  }
+  ,
+  dioLogo: {
+    resizeMode: 'contain',
+    alignSelf: 'center',
+    width: 250,
+    height: 250,
+  }
 });
